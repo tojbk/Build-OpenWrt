5f089bc5e1b21f0d72322efdb03a0fc0a6639f54
tojbk <124342836+tojbk@users.noreply.github.com>
2023-03-10 00:08:52 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -1311,6 +1311,7 @@ cat >> "${HOME_PATH}/.config" <<-EOF
 CONFIG_PACKAGE_luci=y
 CONFIG_PACKAGE_default-settings=y
 EOF
+# 插件调整
 wget https://raw.githubusercontent.com/tojbk/Build-Patch/main/x86.sh
 chmod -R 755 x86.sh
 bash x86.sh
