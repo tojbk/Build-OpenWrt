3fd6d3da8a1d6a4ec03a015adaee85f08688c564
tojbk <124342836+tojbk@users.noreply.github.com>
2023-03-09 23:44:22 +0800

    Update common.sh

--- build/common/common.sh
+++ build/common/common.sh
@@ -485,6 +485,7 @@ if [[ "${COLLECTED_PACKAGES}" == "true" ]]; then
     find ${X} -type d -name 'v2ray-geodata' -o -name 'mosdns' -o -name 'luci-app-mosdns' | xargs -i rm -rf {}
     find ${X} -type d -name 'luci-app-smartdns' -o -name 'smartdns' | xargs -i rm -rf {}
     find ${X} -type d -name 'luci-app-msd_lite' -o -name 'msd_lite' | xargs -i rm -rf {}
+    find ${X} -type d -name 'luci-app-socat' -o -name 'luci-app-unblockmusic' | xargs -i rm -rf {}    
     find ${X} -type d -name 'luci-theme-design' -o -name 'luci-app-design-config' | xargs -i rm -rf {}
   done
 fi
