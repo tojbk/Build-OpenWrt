2a66739d11d7868615becbfbca0a909dfa639683
tojbk <124342836+tojbk@users.noreply.github.com>
2025-02-26 21:37:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+rm -rf feeds/luci/applications/luci-app-openclash
+git clone https://github.com/tojbk/luci-app-openclash feeds/luci/applications/luci-app-openclash
